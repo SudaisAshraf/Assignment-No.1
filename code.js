14062f9ac9f2573518dbdfbd847d91d17f65ce57
Sudais Ashraf <sudais.ashraf.919@gmail.com>
2024-09-27 05:44:48 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,80 @@
+//CHAPTER No.1
+//(ALERT)
+
+//  1#
+alert("Welcome!");
+
+//  2#
+alert("Error! Please enter a valid password");
+
+//  3#
+alert("Welcome to JS Land...\nHappy Coding!")
+
+// 4#
+alert("Welcome to JS land...");
+alert("Happy Coding!");
+
+//---------------------------------------------------------------------------------------------------
+
+
+//CHAPTER No.2
+//(VARIABLES FOR STRINGS)
+
+//            1#
+let username;
+
+//---------------------------------------------------------------------------------------------------
+//            2#
+let myName = "Sudais Ashraf Mughal";
+//---------------------------------------------------------------------------------------------------
+
+//            3#
+// a) 
+let say;
+// b) 
+say = "Hello World";
+// c)
+alert(message);
+
+//---------------------------------------------------------------------------------------------------
+
+//            4#
+let studentName,studentCountry,studentAge,studentHobby;
+
+studentName = prompt("Enter student's name:");
+studentAge = parseInt(prompt("Enter student's age:"));
+studentCountry = prompt("Enter student's country:");
+studentHobby = prompt("Enter student's hobby:");
+
+alert(("Std Name is: ") + studentName);
+alert( ("Std Age is: ")+ studentAge);
+alert(("Std Country is: ") + studentCountry);
+alert(("Std Hobby is: ") + studentHobby);
+
+//---------------------------------------------------------------------------------------------------
+
+//            5#
+for (let i = pizza.length; i > 0; i--) {
+    alert(pizza.substring(0, i));
+  }
+
+ // -------------------------------------------------------------------------------------------------
+
+  //            6#
+let email;
+email= "sudais.ashraf.919@gmail.com";
+let message = "Your email address is: " + email;
+alert(message);
+
+//---------------------------------------------------------------------------------------------------
+
+//            7#
+let book="A smarter way to learn JavaScript";
+alert(book);
+
+//---------------------------------------------------------------------------------------------------
+
+//            8#
+let myLogo = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(myLogo);
+document.write(myLogo);
